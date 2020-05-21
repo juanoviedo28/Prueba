e53e7495a05faa6aeb3265fbc67e5150b35f7904
juanoviedo28 <65684836+juanoviedo28@users.noreply.github.com>
2020-05-20 22:52:04 -0300

    Generando Constructores

--- Cliente1/src/Cliente1/Cliente1.java
+++ Cliente1/src/Cliente1/Cliente1.java
@@ -7,6 +7,14 @@ public class Cliente1 {
       private String Apellido,Nombre,NumeroDocumento,Codigo;
     
     
+       public Cliente1(String Apellido, String Nombre, String NumeroDocumento, String Codigo) {
+        super();
+        this.Apellido = Apellido;
+        this.Nombre = Nombre;
+        this.NumeroDocumento = NumeroDocumento;
+        this.Codigo = Codigo;
+    }
+      
     
     
 }
